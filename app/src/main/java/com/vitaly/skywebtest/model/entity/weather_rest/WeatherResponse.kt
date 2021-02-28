0f7636d1aa7e3a79541d693909e3f224afcb8baa
Vitas1968 <dor1968@mail.ru>
2021-02-28 12:56:46 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/model/entity/weather_rest/WeatherResponse.kt
+++ app/src/main/java/com/vitaly/skywebtest/model/entity/weather_rest/WeatherResponse.kt
@@ -1,19 +1,10 @@
-package com.vitaly.skywebtest.model.entity.weather
+package com.vitaly.skywebtest.model.entity.weather_rest
 
 import com.google.gson.annotations.Expose
 
 data class WeatherResponse(
-        @Expose val coord : Coord,
         @Expose val weather : Array<Weather>,
         @Expose val main : Main,
-        @Expose val visibility : Int,
-        @Expose val wind : Wind,
-        @Expose val clouds : Clouds,
-        @Expose val dt : Long,
-        @Expose val sys : SysWeather,
-        @Expose val timezone : Int,
         @Expose val id : Int,
         @Expose val name : String,
-        @Expose val cod : Int
-
 )
