28a958ff3169b9da6eba188d3901ca0e1303e989
Vitas1968 <dor1968@mail.ru>
2021-02-28 13:08:13 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/utils/ProgressDialogFragment.kt
+++ app/src/main/java/com/vitaly/skywebtest/utils/ProgressDialogFragment.kt
@@ -1,4 +1,19 @@
 package com.vitaly.skywebtest.utils
 
-class ProgressDialogFrament {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.vitaly.skywebtest.R
+
+class ProgressDialogFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dialog, container, false)
+    }
 }
