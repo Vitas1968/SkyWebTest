60fbe47671c0776aa6577b4f215681362164c96e
Vitas1968 <dor1968@mail.ru>
2021-02-28 12:52:56 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/model/datasource/ImageDataSourceFactory.kt
+++ app/src/main/java/com/vitaly/skywebtest/model/datasource/ImageDataSourceFactory.kt
@@ -1,4 +1,21 @@
 package com.vitaly.skywebtest.model.datasource
 
-class ImageDataSourceFactory {
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import com.vitaly.skywebtest.model.entity.ImageDataClass
+import com.vitaly.skywebtest.model.networkservice.IImageRemoteDataSource
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+class ImageDataSourceFactory(
+    private val remoteDataSource: IImageRemoteDataSource,
+    private val compositeDisposable: CompositeDisposable
+) : DataSource.Factory<Int, ImageDataClass>() {
+
+    val imageDataSourceLiveData = MutableLiveData<ImageDataSource>()
+
+    override fun create(): DataSource<Int, ImageDataClass> {
+        val imgDataSource = ImageDataSource(remoteDataSource, compositeDisposable)
+        imageDataSourceLiveData.postValue(imgDataSource)
+        return imgDataSource
+    }
 }
