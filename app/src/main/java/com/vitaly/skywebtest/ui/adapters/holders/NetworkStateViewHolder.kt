bbbc2d842b18f1ef88383b1479053ba4a8b301e3
Vitas1968 <dor1968@mail.ru>
2021-02-28 13:03:39 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/ui/adapters/holders/NetworkStateViewHolder.kt
+++ app/src/main/java/com/vitaly/skywebtest/ui/adapters/holders/NetworkStateViewHolder.kt
@@ -1,4 +1,35 @@
 package com.vitaly.skywebtest.ui.adapters.holders
 
-class NetworkStateViewHolder {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.vitaly.skywebtest.R
+import com.vitaly.skywebtest.utils.State
+import kotlinx.android.synthetic.main.item_image_progress.view.*
+
+class NetworkStateViewHolder(
+    view: View,
+    private val retryCallback: () -> Unit
+) : RecyclerView.ViewHolder(view) {
+    init {
+        itemView.txt_error.setOnClickListener { retryCallback() }
+    }
+
+    fun bindTo(networkState: State) {
+        with(itemView) {
+            txt_error.visibility = if (networkState == State.ERROR) View.VISIBLE else View.GONE
+            progress_progress_bar.visibility =
+                if (networkState != State.LOADING) View.VISIBLE else View.GONE
+
+        }
+    }
+
+    companion object {
+        fun create(parent: ViewGroup, retryCallback: () -> Unit): NetworkStateViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(R.layout.item_image_progress, parent, false)
+            return NetworkStateViewHolder(view, retryCallback)
+        }
+    }
 }
