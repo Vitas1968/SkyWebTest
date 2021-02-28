e34798b96ad2778b263631dd3ec7bd1f6bf28540
Vitas1968 <dor1968@mail.ru>
2021-02-28 13:09:32 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/utils/StringHelper.kt
+++ app/src/main/java/com/vitaly/skywebtest/utils/StringHelper.kt
@@ -1,2 +1,17 @@
 package com.vitaly.skywebtest.utils
 
+fun stringBuilder(arrayString: Array<String> ) : String =
+    StringBuilder()
+        .append("Город: ")
+        .append(arrayString[0])
+        .append("  Температура: ")
+        .append(arrayString[1])
+        .append(" C")
+        .append("  Облачность: ")
+        .append(arrayString[2])
+        .append("  Влажность: ")
+        .append(arrayString[3])
+        .append(" %")
+        .toString()
+
+
