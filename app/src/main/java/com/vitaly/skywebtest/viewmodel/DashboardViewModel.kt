0a0f0b153ff51fdc9d4b08419a7b779843130c17
Vitas1968 <dor1968@mail.ru>
2021-02-28 13:10:55 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/vitaly/skywebtest/viewmodel/DashboardViewModel.kt
@@ -1,13 +1,56 @@
 package com.vitaly.skywebtest.viewmodel
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.vitaly.skywebtest.model.datasource.ImageDataSource
+import com.vitaly.skywebtest.model.datasource.ImageDataSourceFactory
+import com.vitaly.skywebtest.model.entity.ImageDataClass
+import com.vitaly.skywebtest.model.networkservice.IImageRemoteDataSource
+import com.vitaly.skywebtest.utils.State
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
-class DashboardViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+class DashboardViewModel(
+    private val remoteDataSource: IImageRemoteDataSource
+) : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+    val imageList: LiveData<PagedList<ImageDataClass>>
+    private val imageDataSourceFactory: ImageDataSourceFactory
+    private val pageSize = 5
+
+    init {
+        imageDataSourceFactory = createImageDataSourceFactory()
+        val config = createPageListConfig()
+        imageList = LivePagedListBuilder<Int, ImageDataClass>(imageDataSourceFactory, config)
+            .build()
+
     }
-    val text: LiveData<String> = _text
+
+    private fun createImageDataSourceFactory() =
+        ImageDataSourceFactory(remoteDataSource, compositeDisposable)
+
+    private fun createPageListConfig() = PagedList.Config.Builder()
+        .setPageSize(pageSize)
+        .setInitialLoadSizeHint(pageSize * 2)
+        .setEnablePlaceholders(false)
+        .build()
+
+    fun retry() {
+        imageDataSourceFactory.imageDataSourceLiveData.value?.retry()
+    }
+
+    fun getState(): LiveData<State> = Transformations.switchMap<ImageDataSource,
+            State>(imageDataSourceFactory.imageDataSourceLiveData, ImageDataSource::networkState)
+
+    fun listIsEmpty(): Boolean {
+        return imageList.value?.isEmpty() ?: true
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
 }
