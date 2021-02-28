f13b2827d8654f8010aca5b963db6be3fe22c1e0
Vitas1968 <dor1968@mail.ru>
2021-02-28 12:49:00 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/application/SkyWebTestApp.kt
+++ app/src/main/java/com/vitaly/skywebtest/application/SkyWebTestApp.kt
@@ -1,13 +1,21 @@
 package com.vitaly.skywebtest.application
 
 import android.app.Application
+import com.vitaly.skywebtest.di.authModule
+
+
+import com.vitaly.skywebtest.di.photosModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+
 
 class SkyWebTestApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin { androidContext(this@SkyWebTestApp) }
+        startKoin {
+            androidContext(this@SkyWebTestApp)
+            modules(photosModule,authModule)
+        }
     }
 }
