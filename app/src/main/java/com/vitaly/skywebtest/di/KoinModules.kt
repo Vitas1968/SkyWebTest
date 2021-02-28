62d86f5bd858fe76e944cd3e634c65beb2bb8de0
Vitas1968 <dor1968@mail.ru>
2021-02-28 12:50:42 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/di/KoinModules.kt
+++ app/src/main/java/com/vitaly/skywebtest/di/KoinModules.kt
@@ -1,43 +1,89 @@
 package com.vitaly.skywebtest.di
 
+
 import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.vitaly.skywebtest.R
+import com.vitaly.skywebtest.model.datasource.WeatherDataSource
+import com.vitaly.skywebtest.model.networkservice.IImageRemoteDataSource
+import com.vitaly.skywebtest.model.networkservice.IWeatherRemoteDataSource
+import com.vitaly.skywebtest.viewmodel.DashboardViewModel
+import com.vitaly.skywebtest.viewmodel.HomeViewModel
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
 import okhttp3.OkHttpClient
-import org.koin.core.context.loadKoinModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-fun injectDependencies() = loadModules
-    private val loadModules by lazy {
-        loadKoinModules(
-            listOf( )
-        )
-    }
 
-val remoteModule = module {
-
-    val BASE_URL = "http://storyheroes.online/api/"
-
-    single {
-        OkHttpClient.Builder().build()
-    }
-
-    single {
+val photosModule = module {
+    single(named("PHOTOS_BASE_URL")) { androidContext().getString(R.string.base_url_image) }
+    single<Gson> {
         GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.IDENTITY)
             .excludeFieldsWithoutExposeAnnotation()
             .create()
     }
-
-    single {
-        Retrofit.Builder()
-            //.baseUrl(BASE_URL)
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create(get()))
-            .client(get())
-            //.build()
-            //.create(IRemoteDataSource::class.java)
+    single<IImageRemoteDataSource>(named("PhotosWebService")) {
+        createWebService(
+            okHttpClient = createPhotosOkHttpClient(),
+            baseUrl = get(named("PHOTOS_BASE_URL")),
+            gson = get()
+        )
     }
+    viewModel { DashboardViewModel(get(named("PhotosWebService"))) }
 }
+
+val authModule = module {
+    single(named("base_url_weather")) { androidContext().getString(R.string.base_url_weather) }
+
+    single<IWeatherRemoteDataSource>(named("WeatherWebService")) {
+
+        createWebService(
+            okHttpClient = createWeathersOkHttpClient(),
+            baseUrl = get(named("base_url_weather")),
+            gson = get()
+        )
+    }
+    single { WeatherDataSource(get(named("WeatherWebService"))) }
+    viewModel { HomeViewModel(get()) }
+}
+
+fun createPhotosOkHttpClient() = OkHttpClient.Builder().build()
+
+fun createWeathersOkHttpClient(): OkHttpClient {
+    val client = OkHttpClient.Builder()
+    return client.addInterceptor { chain ->
+        var request = chain.request()
+        val url = request.url()
+            .newBuilder()
+            .addQueryParameter("appid", "c35880b49ff95391b3a6d0edd0c722eb")
+            .build()
+        request = request.newBuilder()
+            .url(url)
+            .build()
+        return@addInterceptor chain.proceed(request)
+    }.build()
+}
+
+inline fun <reified T> createWebService(
+    okHttpClient: OkHttpClient,
+    baseUrl: String,
+    gson: Gson
+): T {
+
+    val rFit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(okHttpClient)
+        .build()
+    return rFit.create(T::class.java)
+
+}
+
+
