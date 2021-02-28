26bbbc23ea255b19002b346e7083f5639f8676b7
Vitas1968 <dor1968@mail.ru>
2021-02-28 12:53:41 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/model/datasource/WeatherDataSource.kt
+++ app/src/main/java/com/vitaly/skywebtest/model/datasource/WeatherDataSource.kt
@@ -1,4 +1,25 @@
 package com.vitaly.skywebtest.model.datasource
 
-class WeatherDataSource {
+import com.vitaly.skywebtest.model.entity.WeatherReady
+import com.vitaly.skywebtest.model.networkservice.IWeatherRemoteDataSource
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class WeatherDataSource(
+    private val weatherWebService: IWeatherRemoteDataSource
+) {
+    fun getWeather(): Single<WeatherReady> =
+        weatherWebService.getWeather()
+            .flatMap {
+                val weatherReady = WeatherReady(
+                    cityName = it.name,
+                    cityId = it.id,
+                    temperature = it.main.temp,
+                    humidity = it.main.humidity,
+                    description = it.weather[0].description
+                )
+                Single.just(weatherReady)
+            }.subscribeOn(Schedulers.io())
+
+
 }
