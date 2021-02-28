5e4e956e7708607967b46a8d31f7f966126381ef
Vitas1968 <dor1968@mail.ru>
2021-02-28 12:55:08 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/model/entity/WeatherReady.kt
+++ app/src/main/java/com/vitaly/skywebtest/model/entity/WeatherReady.kt
@@ -1,3 +1,9 @@
 package com.vitaly.skywebtest.model.entity
 
-data class WeatherReady()
+data class WeatherReady(
+        val cityName: String = "No city",
+        val cityId: Int = -1,
+        val temperature: Int,
+        val humidity: Int,
+        val description: String = "No description"
+)
