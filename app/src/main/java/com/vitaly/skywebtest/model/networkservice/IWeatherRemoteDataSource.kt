25098bfe57b6a320eea72193ee655c322dc1bd11
Vitas1968 <dor1968@mail.ru>
2021-02-28 12:57:38 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/model/networkservice/IWeatherRemoteDataSource.kt
+++ app/src/main/java/com/vitaly/skywebtest/model/networkservice/IWeatherRemoteDataSource.kt
@@ -1,8 +1,10 @@
-package com.vitaly.skywebtest.model.datasource
+package com.vitaly.skywebtest.model.networkservice
 
+import com.vitaly.skywebtest.model.entity.weather_rest.WeatherResponse
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 
 interface IWeatherRemoteDataSource {
     @GET("weather?id=553915&lang=ru&units=metric")
-    fun getWeather()
+    fun getWeather(): Single<WeatherResponse>
 }
