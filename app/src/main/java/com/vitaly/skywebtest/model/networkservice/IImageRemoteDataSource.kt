7c80bc7c0ea7bd986ca88a7382caa84f4d3d0601
Vitas1968 <dor1968@mail.ru>
2021-02-28 12:57:15 +0700

    реализация класса

--- app/src/main/java/com/vitaly/skywebtest/model/networkservice/IImageRemoteDataSource.kt
+++ app/src/main/java/com/vitaly/skywebtest/model/networkservice/IImageRemoteDataSource.kt
@@ -1,4 +1,5 @@
-package com.vitaly.skywebtest.model.datasource
+package com.vitaly.skywebtest.model.networkservice
+
 
 import com.vitaly.skywebtest.model.entity.ImageDataClass
 import io.reactivex.rxjava3.core.Single
@@ -8,7 +9,7 @@ import retrofit2.http.Query
 interface IImageRemoteDataSource {
     @GET("list")
     fun getImage(
-            @Query ("page") page:Int,
-            @Query ("limit") limit:Int
+            @Query("page") page: Int,
+            @Query("limit") limit: Int
     ): Single<List<ImageDataClass>>
 }
