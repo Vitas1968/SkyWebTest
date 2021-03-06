6f7e6c506e8e8cd0406a064d0d60dc4b65849cc3
Vitas1968 <dor1968@mail.ru>
2021-03-06 10:38:41 +0700

    актуализация data класса

--- app/src/main/java/com/vitaly/skywebtest/model/entity/weather_rest/WeatherResponse.kt
+++ app/src/main/java/com/vitaly/skywebtest/model/entity/weather_rest/WeatherResponse.kt
@@ -1,10 +1,8 @@
 package com.vitaly.skywebtest.model.entity.weather_rest
 
-import com.google.gson.annotations.Expose
 
 data class WeatherResponse(
-        @Expose val weather : Array<Weather>,
-        @Expose val main : Main,
-        @Expose val id : Int,
-        @Expose val name : String,
+         val weather : Array<Weather>,
+         val main : Main,
+         val name : String,
 )
